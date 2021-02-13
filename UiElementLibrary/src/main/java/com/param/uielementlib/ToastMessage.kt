46e3c6a0239045093142103aa17bcda65050aab9
parmeshtoyou <parmeshtoyou@gmail.com>
2021-02-13 22:37:40 +0530

    updated ToastMessage class to an Object

--- UiElementLibrary/src/main/java/com/param/uielementlib/ToastMessage.kt
+++ UiElementLibrary/src/main/java/com/param/uielementlib/ToastMessage.kt
@@ -3,8 +3,8 @@ package com.param.uielementlib
 import android.content.Context
 import android.widget.Toast
 
-class ToastMessage {
-    fun small(context: Context, message: String) {
+object ToastMessage {
+    fun s(context: Context, message: String) {
         Toast.makeText(context, message, Toast.LENGTH_LONG).show()
     }
 }
--- app/src/main/java/com/param/uielement/MainActivity.kt
+++ app/src/main/java/com/param/uielement/MainActivity.kt
@@ -2,10 +2,12 @@ package com.param.uielement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.param.uielementlib.ToastMessage
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ToastMessage().small(this, "Hello Library")
     }
 }
